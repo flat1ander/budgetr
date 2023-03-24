7d51ee3660288ae107b6afe98ce800c848c1d863
flat1ander <thematthewlane@gmail.com>
2023-03-24 11:56:47 -0400

    Completed all routes with working new, post and show.

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const app = express();
 const port = 4000
+const budget = require('./models/budget');
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended:false }));
+app.use(express.static('public'));
 
 app.get('/budgets', (req, res) => {
-    res.send("Hello World")
+    res.render('index.ejs', {budget})
 })
 
+app.get('/budgets/new', (req, res) => {
+    res.render('new.ejs')
+})
 
+app.get('/budgets/:index', (req, res) => {
+    const budgetItem = budget[req.params.index]
+    res.render('show.ejs', {budgetItem})
+})
 
-
+app.post('/budgets', (req, res) => {
+    budget.unshift(req.body);
+    res.redirect('/budgets')
+})
 
 
 
